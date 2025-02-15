add1845dc275123f7776ae81b10e6e095fa33df6
Pham Duc Duy <134459693+phamduyaaaa@users.noreply.github.com>
2025-02-15 15:56:32 +0700

    batch, group, layer

--- model.py
+++ model.py
@@ -1,36 +1,47 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
 
 class VoxNet(nn.Module):
-    def __init__(self, num_classes=10):
-        super().__init__()
+    def __init__(self, norm_type="groupnorm"):  # "batchnorm", "groupnorm", "layernorm"
+        super(VoxNet, self).__init__()
+        self.conv1 = nn.Conv3d(1, 32, kernel_size=5, stride=2)
+        self.conv2 = nn.Conv3d(32, 64, kernel_size=3, stride=1)
 
-        self.conv1 = nn.Conv3d(1, 32, kernel_size=5, stride=2)  # (32, 14, 14, 14)
-        self.conv2 = nn.Conv3d(32, 64, kernel_size=3, stride=1)  # (64, 12, 12, 12)
-        self.batchnorm1 = nn.BatchNorm3d(32)
-        self.batchnorm2 = nn.BatchNorm3d(64)
+        if norm_type == "batchnorm":
+            self.norm1 = nn.BatchNorm3d(32)
+            self.norm2 = nn.BatchNorm3d(64)
+        elif norm_type == "groupnorm":
+            self.norm1 = nn.GroupNorm(4, 32)  # 4 nhóm cho 32 kênh
+            self.norm2 = nn.GroupNorm(4, 64)  # 4 nhóm cho 64 kênh
+        elif norm_type == "layernorm":
+            self.norm1 = nn.LayerNorm([32, 14, 14, 14])  # Cần đúng shape, có thể phải chỉnh lại sau
+            self.norm2 = nn.LayerNorm([64, 12, 12, 12])  # Cần đúng shape, kiểm tra lại sau
+            
         self.dropout = nn.Dropout(p=0.3)
-        self.maxpooling = nn.MaxPool3d(kernel_size=2, stride=2)  # (64, 6, 6, 6)
+        self.maxpool = nn.MaxPool3d(kernel_size=2, stride=2)
 
-        # Fully Connected Layers
-        self.fc1 = nn.Linear(64 * 6 * 6 * 6, 1024)  # (1024)
-        self.fc2 = nn.Linear(1024, num_classes)  # (num_classes)
+        self.fc1 = nn.Linear(13824, 1024)
+        self.fc2 = nn.Linear(1024, 10)
 
     def forward(self, x):
-        x = F.relu(self.batchnorm1(self.conv1(x)))  # Conv1 -> ReLU -> BatchNorm
-        x = F.relu(self.batchnorm2(self.conv2(x)))  # Conv2 -> ReLU -> BatchNorm
-        #x = F.relu(self.conv1(x))
-        #x = F.relu(self.conv2(x))
-        x = self.maxpooling(x)  # MaxPool
-        x = self.dropout(x)  # Dropout
-        x = torch.flatten(x, start_dim=1)
-        x = F.relu(self.fc1(x))  # FC1 -> ReLU
+        x = self.conv1(x)
+        x = self.norm1(x)  # Áp dụng chuẩn hóa
+        x = nn.ReLU()(x)
+
+        x = self.conv2(x)
+        x = self.norm2(x)  # Áp dụng chuẩn hóa
+        x = nn.ReLU()(x)
+
+        x = self.maxpool(x)
         x = self.dropout(x)
+
+        x = x.view(x.size(0), -1)  # Flatten
+        x = self.fc1(x)
+        x = nn.ReLU()(x)
+
         x = self.fc2(x)
         return x
 
-model1 = VoxNet()
-print(model1.fc1.weight.data.clone())
-torch.save(model1.state_dict(),"init_weights.pth")
+
+#model1 = VoxNet()
+#print(model1.fc1.weight.data.clone())
+#torch.save(model1.state_dict(),"init_weights.pth")
